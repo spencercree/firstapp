e89837dcc15f1ce79fb1db767aff8ab41ebe403d
Spencer McCullough <spencermccullough@Spencers-MacBook-Pro.local>
2014-04-24 15:46:09 -0400

    Initial Commit

---
+++ newermagic8/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  newermagic8
+//
+//  Created by Spencer McCullough on 4/24/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ newermagic8/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  newermagic8
+//
+//  Created by Spencer McCullough on 4/24/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
